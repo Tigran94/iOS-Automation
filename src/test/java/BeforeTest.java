5e42abe5d8f302ca4ccbdd07fd04dea1b683a0b4
TigranYeritsyan <sonagrieg@gmail.com>
2018-07-20 23:26:04 +0400

    Appium server starting and Android emulator starting automaticly

--- src/test/java/BeforeTest.java
+++ src/test/java/BeforeTest.java
@@ -6,15 +6,13 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BeforeTest {
     private String apkDir = System.getProperty("apkDir");
     private String deviceName = System.getProperty("deviceName");
-    private String version = System.getProperty("version");
-    private String automatioName = System.getProperty("automationName");
-    private boolean fullResrt = Boolean.getBoolean(System.getProperty("fullReset"));
 
 
     private static AndroidDriver driver;
@@ -22,17 +20,43 @@ public class BeforeTest {
     @BeforeClass
     public void initiateDriver(){
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName","Android");
+
+        desiredCapabilities.setCapability("deviceReadyTimeout",10);
+        desiredCapabilities.setCapability("avd",deviceName.replace(" ","_"));
         desiredCapabilities.setCapability("deviceName",deviceName);
         desiredCapabilities.setCapability("app",System.getProperty("user.dir")+"/apk/Todoist_v12.8_apkpure.com.apk");
 //        desiredCapabilities.setCapability("fullReset",fullResrt);
         desiredCapabilities.setCapability("appWaitActivity", "*");
 
         try {
-
+            startServer();
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
         }catch (MalformedURLException e){
             e.fillInStackTrace();
+        }
+    }
+
+
+    public void startServer(){
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec("cmd.exe /c start cmd.exe /k \"appium -a 127.0.0.1 -p 4723 --session-override -dc \"{\"\"noReset\"\": \"\"false\"\"}\"\"");
+            Thread.sleep(10000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (InterruptedException e){
+            e.fillInStackTrace();
+        }
+    }
+
+
+    public void quitTask(){
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec("taskkill /F /IM node.exe");
+            runtime.exec("taskkill /F /IM cmd.exe");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -49,6 +73,7 @@ public class BeforeTest {
 
     @AfterClass
     public void closeAppium(){
+        quitTask();
         //driver.closeApp();
 //        driver.close();
     }
